3724dd67ee1948952adbecbf246b3ec7bece96ea
Yura <yura05061997@gmail.com>
2020-03-25 14:16:28 +0000

    add accordion

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-let dialog = document.querySelector('dialog');
-   document.querySelector('#openDialog').onclick = function() {
-    dialog.show(); 
-   }
-   document.querySelector('#closeDialog').onclick = function() {
-    dialog.close();
-   }
+// let dialog = document.querySelector('dialog');
+//    document.querySelector('#openDialog').onclick = function() {
+//     dialog.show(); 
+//    }
+//    document.querySelector('#closeDialog').onclick = function() {
+//     dialog.close();
+//    }
 
 
 
-$('.click-jq').click(() => {
-    $('p').toggle(1000);
-});  
+// $('.click-jq').click(() => {
+//     $('p').toggle(1000);
+// });  
+ 
+ ! function(i) {
+ let o, n;
+ i(".title_block").on("click", function() {
+ o = i(this).parents(".accordion_item"), n = o.find(".info"),
+ o.hasClass("active_block") ? (o.removeClass("active_block"),
+ n.slideUp()) : (o.addClass("active_block"), n.stop(!0, !0).slideDown(),
+ o.siblings(".active_block").removeClass("active_block").children(
+ ".info").stop(!0, !0).slideUp())
+ })
+ }(jQuery);
+
